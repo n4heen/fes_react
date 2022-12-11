34ae484e29d0df2e7f1b97e079e57d235e28edf3
Naheen Habib <naheenhabib@outlook.com>
2022-12-11 20:49:52 +1100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import Todo from "./components/Todo";
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>My Todo List</h1>
+      <Todo
+        header='Finish FES'
+      />
+      <Todo
+        header='Finish Interview Section'
+      />
+      <Todo
+        header='Land a thing'
+      />
+
     </div>
   );
 }
