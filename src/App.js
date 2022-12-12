6a010689a371d0041dbceb5d2f4987ee73d4a51d
Naheen Habib <naheenhabib@outlook.com>
2022-12-12 14:59:21 +1100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,62 +1,12 @@
-import { useEffect, useState } from "react";
-import Modal from "./components/Modal";
-import Title from "./components/Title";
-import Todo from "./components/Todo";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
-  const [toggleModal, setToggleModal] = useState(false)
-
-  function deleteToDo() {
-    setToggleModal(true)
-    console.log('egg')
-  }
-
-  function cancel() {
-    setToggleModal(false)
-  }
-
-  useEffect(() => {
-
-  }, [])
 
 
   return (
     <div className="App">
 
-      <Title />
-      <>
-        <input type="text" onChange={(event) => {
-          console.log(event.target.value)
-        }} />
-        <button>Add todo</button>
-      </>
-      <div className="todo__wrapper">
-        <Todo
-          title='Finish FES'
-          para="Join a dynamically growing hacking community and take your cybersecurity skills to the next level through the most captivating, gamified, hands-on training experience!"
-          deleteToDo={deleteToDo}
-
-        />
-        <Todo
-          title='Finish Interview Section'
-          para="Join a dynamically growing hacking community and take your cybersecurity skills to the next level through the most captivating, gamified, hands-on training experience!"
-          deleteToDo={deleteToDo}
-
-        />
-        <Todo
-          title='Land a thing'
-          para="Join a dynamically growing hacking community and take your cybersecurity skills to the next level through the most captivating, gamified, hands-on training experience!"
-          deleteToDo={deleteToDo}
-
-
-        />
-      </div>
-      {toggleModal && <Modal
-        modalPara="Are you sure?"
-        cancel={cancel}
-      />}
 
     </div>
   );
