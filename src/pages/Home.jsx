efe62b994c229cbeb1e45154f98681ecfd7ade6f
Naheen Habib <naheenhabib@outlook.com>
2022-12-12 15:58:55 +1100

    new commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 export default function Home() {
 
-
+    const [users, setUsers] = useState([])
     async function fetchUser() {
         const { data } = await axios.get("https://jsonplaceholder.typicode.com/users")
+        setUsers(data)
         console.log(data)
     }
 
@@ -16,6 +17,6 @@ export default function Home() {
     }, [])
 
     return (
-        <div>Home</div>
+        <div>{users}</div>
     )
 }
