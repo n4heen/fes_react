488261af723ebbb6d7d0d143954a122ce61dc914
Naheen Habib <naheenhabib@outlook.com>
2022-12-12 15:15:11 +1100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
 
 function App() {
 
 
 
   return (
-    <div className="App">
+    <div>
+      <Router>
 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
 
     </div>
   );
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Nav() {
+    return (
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+        </nav>
+    )
+}
